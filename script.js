9b3d073bf6b6906df25ba2b3a6feadb5d1b50267
Brosey27 <140609566+Brosey27@users.noreply.github.com>
2023-08-13 13:18:49 +1000

    Add a probability that red bubbles will appear and if clicked will reset the game and alert the user that the current game is over

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ let timeLeft = 30;
 let isGameRunning = false;
 
 function createBubble() {
-  const bubble = document.createElement('div');
-  bubble.classList.add('bubble');
+    const bubble = document.createElement('div');
+    bubble.classList.add('bubble');
+  
+    if (Math.random() < 0.1) {
+      bubble.classList.add('red-bubble');
+    }
 
   
 
@@ -25,21 +29,43 @@ function createBubble() {
 }
 
 function popBubble(bubble) {
-    bubble.removeEventListener('click', () => {
-      popBubble(bubble);
-    });
-    bubble.classList.add('popped'); // Apply the "popped" class
-    score++;
-    updateScore();
-    setTimeout(() => {
-      gameContainer.removeChild(bubble);
-    }, 300); // Remove the bubble after a short delay
+    if (bubble.classList.contains('red-bubble')) {
+      gameOver();
+    } else {
+      bubble.removeEventListener('click', () => {
+        popBubble(bubble);
+      });
+      bubble.classList.add('popped'); // Apply the "popped" class
+      score++;
+      updateScore();
+      setTimeout(() => {
+        gameContainer.removeChild(bubble);
+      }, 300); // Remove the bubble after a short delay
+    }
+  }
+  
+  function gameOver() {
+    isGameRunning = false;
+    alert('Game Over! Your final score: ' + score);
+    resetGame();
   }
 
 function updateScore() {
   const scoreDisplay = document.querySelector('.score');
   scoreDisplay.textContent = `Score: ${score}`;
 }
+
+function resetGame() {
+    const bubbles = document.querySelectorAll('.bubble');
+    bubbles.forEach(bubble => {
+      gameContainer.removeChild(bubble);
+    });
+    score = 0;
+    updateScore();
+  }
+
+
+
 
 
 
